6e768563e21679e3b6503098c7dbdc981051106a
Diego Rodrigues <diegurm@hotmail.com>
2019-10-03 23:04:41 -0300

    Criado SpotController, criar e listar empresas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const routes = express.Router();
 
 const SessionController = require('./controllers/SessionController');
+const SpotsController = require('./controllers/SpotController');
 
 routes.post('/sessions', SessionController.store);
 
+routes.get('/spots', SpotsController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotsController.store);
 
 module.exports = routes;
